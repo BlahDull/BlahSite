81ece5f80de83ac7eb8a5524573af5e50c507075
BlahDull <christophertmontoya@gmail.com>
2024-07-05 00:02:14 -0600

    Added some things

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -13,6 +14,13 @@ def about_page():
 @app.route("/chat.html")
 def chat_page():
     return render_template("chat.html")
+
+@app.route("/chat.html", methods=['POST'])
+def save_message():
+    msg = request.form['message']
+    with open("messages.txt", "a") as file:
+        file.write(f"{datetime.now()}: {msg}\n")
+    return jsonify({'message': 'Message received'})
 
 @app.route("/login.html")
 def login_page():
--- static/scripts.js
+++ static/scripts.js
@@ -1,3 +1,14 @@
-function generate_primes(n) {
-    
+function send_message() {
+    let inputBox = document.getElementById("input_box");
+    let messageBox = document.getElementById("message_box");
+    let message = inputBox.value;
+    let currentContents = messageBox.value;
+    console.log(message);
+    inputBox.value = "";
+    messageBox.value = currentContents + message + "\n"
+    let formData = new FormData;
+    formData.set("message", message)
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/chat.html", true);
+    xhr.send(formData);
 }
